c0fac18e0427574effdd16346098948be240690d
Tw <tw19881113@gmail.com>
2012-10-26 10:23:39 +0800

    add cgrep

---
+++ myself/cgrep/cgrep.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"log"
+	"runtime"
+	"os"
+	"bufio"
+	"bytes"
+	"io"
+	"path/filepath"
+)
+
+var workers = runtime.NumCPU()
+
+type Job struct {
+	filename	string
+	results		chan<- Result
+}
+
+func (job Job) Do(lineRx *regexp.Regexp) {
+	file, err := os.Open(job.filename)
+	if err != nil {
+		log.Printf("error: %s\n", err)
+		return
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for lino := 1; ; lino++ {
+		line, err := reader.ReadBytes('\n')
+		line = bytes.TrimRight(line, "\n\r")
+		if lineRx.Match(line) {
+			job.results <- Result{job.filename, lino, string(line)}
+		}
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("error:%d: %s\n", lino, err)
+			}
+			break;
+		}
+	}
+}
+
+type Result struct {
+	filename	string
+	lino		int
+	line		string
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	if len(os.Args) < 3 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Printf("usage: %s <regexp> <files>\n",
+			filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+	if lineRx, err := regexp.Compile(os.Args[1]); err != nil {
+		log.Fatalf("invalid regexp: %s\n", err)
+	} else {
+		grep(lineRx, commandLineFiles(os.Args[2:]))
+	}
+}
+
+func commandLineFiles(files []string) []string {
+	if runtime.GOOS == "windows" {
+		args := make([]string, 0, len(files))
+		for _, name := range files {
+			if matches, err := filepath.Glob(name); err != nil {
+				args = append(args, name)
+			} else if matches != nil {
+				args = append(args, matches...)
+			}
+		}
+		return args
+	}
+	return files
+}
+
+func grep(lineRx *regexp.Regexp, filenames []string) {
+	jobs := make(chan Job, workers)
+	results := make(chan Result, minimum(1000, len(filenames)))
+	done := make(chan struct{}, workers)
+
+	go addJobs(jobs, filenames, results)
+	for i := 0; i < workers; i++ {
+		go doJobs(done, lineRx, jobs)
+	}
+	go awaitCompletion(done, results)
+	processResults(results)
+}
+
+func minimum(a int, bs ...int) int {
+	for _, b := range bs {
+		if b < a {
+			a = b
+		}
+	}
+	return a
+}
+
+func addJobs(jobs chan<- Job, filenames []string, results chan<- Result) {
+	for _, filename := range filenames {
+		jobs <- Job{filename, results}
+	}
+	close(jobs)
+}
+
+func doJobs(done chan<- struct{}, lineRx *regexp.Regexp, jobs <-chan Job) {
+	for job := range jobs {
+		job.Do(lineRx)
+	}
+	done <- struct{}{}
+}
+
+func awaitCompletion(done <-chan struct{}, results chan Result) {
+	for i := 0; i < workers; i++ {
+		<-done
+	}
+	close(results)
+}
+
+func processResults(results <-chan Result) {
+	for result := range results {
+		fmt.Printf("%s:%d:%s\n", result.filename, result.lino, result.line)
+	}
+}
